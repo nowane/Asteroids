fae2c44266c898dae734acaea46dc845400d86ef
Yorick van den Hurk <nowane@gmail.com>
2021-07-11 22:28:59 +0000

    Fix email.js functionality

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,8 +1,16 @@
 function sendMail(contact) {
     emailjs.send("gmail", "template_ms2", {
-        "from_name": ,
-        "from_email": ,
-        "to_name": ,
-        "message": ,
-    })
+            "from_name": contact.fullname.value,
+            "from_email": contact.emailaddress.value,
+            "message": contact.message.value,
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block loading a new page after submit
 }
