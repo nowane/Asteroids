b0554823d13caea76838a121e8bbb800dd8cf921
Yorick van den Hurk <nowane@gmail.com>
2021-07-12 20:58:25 +0000

    Add keyboard on screen

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,1 +1,19 @@
-validateGuessForm()
+// Create keyboard buttons on-screen
+function generateButtons() {
+  let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+    `
+      <button 
+        class="btn-general mx-auto"
+        id='` + letter + `'
+        onClick="handleGuess('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');
+
+  document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
+
+
+
+generateButtons();
