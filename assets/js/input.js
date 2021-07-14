7d4c61dfc0adc63d62fcf08a9417e5885ffdb758
Yorick van den Hurk <nowane@gmail.com>
2021-07-14 13:30:37 +0000

    Comment code

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,41 +1,48 @@
-// Create keyboard buttons on-screen
+// Based on https://www.youtube.com/watch?v=dgvyE1sJS3Y  
+// Create keyboard buttons on screen
+// Split the function in three, to create a QWERTY-keyboard layout over three different elements
 function generateButtons() {
+  // Top row keyboard 
   let buttonsTopHTML = 'QWERTYUIOP'.split('').map(letter =>
     `
       <button 
-        class="btn-general mx-auto"
+        class="btn-general"
         id='` + letter + `'
         onClick="handleGuess('` + letter + `')"
       >
         ` + letter + `
       </button>
-    `).join('');
+    `).join(''); // Remove commas between words
 
+  // Middle row keyboard 
   let buttonsMiddleHTML = 'ASDFGHJKL'.split('').map(letter =>
     `
       <button 
-        class="btn-general mx-auto"
+        class="btn-general"
         id='` + letter + `'
         onClick="handleGuess('` + letter + `')"
       >
         ` + letter + `
       </button>
-    `).join('');
+    `).join(''); // Remove commas between words
 
+  // Bottom row keyboard 
   let buttonsBottomHTML = 'ZXCVBNM'.split('').map(letter =>
     `
       <button 
-        class="btn-general mx-auto"
+        class="btn-general"
         id='` + letter + `'
         onClick="handleGuess('` + letter + `')"
       >
         ` + letter + `
       </button>
-    `).join('');
+    `).join(''); // Remove commas between words
 
+  // Insert in the HTML 
   document.getElementById('generateTopRow').innerHTML = buttonsTopHTML;
   document.getElementById('generateMiddleRow').innerHTML = buttonsMiddleHTML;
   document.getElementById('generateBottomRow').innerHTML = buttonsBottomHTML;
 }
 
+// Excecute function - generate keyboard
 generateButtons();
