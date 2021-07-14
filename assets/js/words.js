cc6b7804929af44dca290266b7280f1e9e24bfe1
Yorick van den Hurk <nowane@gmail.com>
2021-07-14 14:17:46 +0000

    Add countries in normal js object format

--- assets/js/words.js
+++ assets/js/words.js
@@ -1,162 +1,160 @@
-const animals = [
+const animals = [{
+        word: "lion",
+        difficulty: "easy",
+        category: "animals"
+    },
     {
-    word: "lion",
-    difficulty: "easy",
-    category: "animals"
-},
-{
-    word: "sheep",
-    difficulty: "easy",
-    category: "animals"
-},
-{
-    word: "wolf",
-    difficulty: "easy",
-    category: "animals"
-},
-{
-    word: "tiger",
-    difficulty: "easy",
-    category: "animals"
-},
-{
-    word: "dog",
-    difficulty: "easy",
-    category: "animals"
-},
-{
-    word: "ant",
-    difficulty: "easy",
-    category: "animals"
-},
-{
-    word: "cat",
-    difficulty: "easy",
-    category: "animals"
-},
-{
-    word: "fox",
-    difficulty: "easy",
-    category: "animals"
-},
-{
-    word: "pig",
-    difficulty: "easy",
-    category: "animals"
-}, {
-    word: "cow",
-    difficulty: "easy",
-    category: "animals"
-},
-{
-    word: "bird",
-    difficulty: "easy",
-    category: "animals"
-},
-{
-    word: "chicken",
-    difficulty: "medium",
-    category: "animals"
-},
-{
-    word: "monkey",
-    difficulty: "medium",
-    category: "animals"
-},
-{
-    word: "rabbit",
-    difficulty: "medium",
-    category: "animals"
-},
-{
-    word: "camel",
-    difficulty: "medium",
-    category: "animals"
-},
-{
-    word: "cheetah",
-    difficulty: "medium",
-    category: "animals"
-},
-{
-    word: "coyote",
-    difficulty: "medium",
-    category: "animals"
-},
-{
-    word: "eagle",
-    difficulty: "medium",
-    category: "animals"
-},
-{
-    word: "elephant",
-    difficulty: "medium",
-    category: "animals"
-},
-{
-    word: "giraffe",
-    difficulty: "medium",
-    category: "animals"
-},
-{
-    word: "hamster",
-    difficulty: "medium",
-    category: "animals"
-},
-{
-    word: "grasshopper",
-    difficulty: "hard",
-    category: "animals"
-},
-{
-    word: "armadillo",
-    difficulty: "hard",
-    category: "animals"
-},
-{
-    word: "caribou",
-    difficulty: "hard",
-    category: "animals"
-},
-{
-    word: "cockroach",
-    difficulty: "hard",
-    category: "animals"
-},
-{
-    word: "hedgehog",
-    difficulty: "hard",
-    category: "animals"
-},
-{
-    word: "hippopotamus",
-    difficulty: "hard",
-    category: "animals"
-},
-{
-    word: "hummingbird",
-    difficulty: "hard",
-    category: "animals"
-},
-{
-    word: "nightingale",
-    difficulty: "hard",
-    category: "animals"
-},
-{
-    word: "porcupine",
-    difficulty: "hard",
-    category: "animals"
-},
-{
-    word: "rhinoceros",
-    difficulty: "hard",
-    category: "animals"
-},
+        word: "sheep",
+        difficulty: "easy",
+        category: "animals"
+    },
+    {
+        word: "wolf",
+        difficulty: "easy",
+        category: "animals"
+    },
+    {
+        word: "tiger",
+        difficulty: "easy",
+        category: "animals"
+    },
+    {
+        word: "dog",
+        difficulty: "easy",
+        category: "animals"
+    },
+    {
+        word: "ant",
+        difficulty: "easy",
+        category: "animals"
+    },
+    {
+        word: "cat",
+        difficulty: "easy",
+        category: "animals"
+    },
+    {
+        word: "fox",
+        difficulty: "easy",
+        category: "animals"
+    },
+    {
+        word: "pig",
+        difficulty: "easy",
+        category: "animals"
+    }, {
+        word: "cow",
+        difficulty: "easy",
+        category: "animals"
+    },
+    {
+        word: "bird",
+        difficulty: "easy",
+        category: "animals"
+    },
+    {
+        word: "chicken",
+        difficulty: "medium",
+        category: "animals"
+    },
+    {
+        word: "monkey",
+        difficulty: "medium",
+        category: "animals"
+    },
+    {
+        word: "rabbit",
+        difficulty: "medium",
+        category: "animals"
+    },
+    {
+        word: "camel",
+        difficulty: "medium",
+        category: "animals"
+    },
+    {
+        word: "cheetah",
+        difficulty: "medium",
+        category: "animals"
+    },
+    {
+        word: "coyote",
+        difficulty: "medium",
+        category: "animals"
+    },
+    {
+        word: "eagle",
+        difficulty: "medium",
+        category: "animals"
+    },
+    {
+        word: "elephant",
+        difficulty: "medium",
+        category: "animals"
+    },
+    {
+        word: "giraffe",
+        difficulty: "medium",
+        category: "animals"
+    },
+    {
+        word: "hamster",
+        difficulty: "medium",
+        category: "animals"
+    },
+    {
+        word: "grasshopper",
+        difficulty: "hard",
+        category: "animals"
+    },
+    {
+        word: "armadillo",
+        difficulty: "hard",
+        category: "animals"
+    },
+    {
+        word: "caribou",
+        difficulty: "hard",
+        category: "animals"
+    },
+    {
+        word: "cockroach",
+        difficulty: "hard",
+        category: "animals"
+    },
+    {
+        word: "hedgehog",
+        difficulty: "hard",
+        category: "animals"
+    },
+    {
+        word: "hippopotamus",
+        difficulty: "hard",
+        category: "animals"
+    },
+    {
+        word: "hummingbird",
+        difficulty: "hard",
+        category: "animals"
+    },
+    {
+        word: "nightingale",
+        difficulty: "hard",
+        category: "animals"
+    },
+    {
+        word: "porcupine",
+        difficulty: "hard",
+        category: "animals"
+    },
+    {
+        word: "rhinoceros",
+        difficulty: "hard",
+        category: "animals"
+    },
 ]
 
-const foods = [
-    {
+const foods = [{
         word: "chips",
         difficulty: "easy",
         category: "food"
@@ -305,5 +303,157 @@ const foods = [
         word: "bourguignon",
         difficulty: "hard",
         category: "food"
+    },
+]
+
+const countries = [{
+        word: "Togo",
+        difficulty: "easy",
+        category: "countries"
+    },
+    {
+        word: "Fiji",
+        difficulty: "easy",
+        category: "countries"
+    },
+    {
+        word: "Spain",
+        difficulty: "easy",
+        category: "countries"
+    },
+    {
+        word: "Peru",
+        difficulty: "easy",
+        category: "countries"
+    },
+    {
+        word: "Oman",
+        difficulty: "easy",
+        category: "countries"
+    },
+    {
+        word: "Niger",
+        difficulty: "easy",
+        category: "countries"
+    },
+    {
+        word: "Nepal",
+        difficulty: "easy",
+        category: "countries"
+    },
+    {
+        word: "Mali",
+        difficulty: "easy",
+        category: "countries"
+    },
+    {
+        word: "Malta",
+        difficulty: "easy",
+        category: "countries"
+    },
+    {
+        word: "Iran",
+        difficulty: "easy",
+        category: "countries"
+    },
+    {
+        word: "Algeria",
+        difficulty: "medium",
+        category: "countries"
+    },
+    {
+        word: "Albania",
+        difficulty: "medium",
+        category: "countries"
+    },
+    {
+        word: "Belgium",
+        difficulty: "medium",
+        category: "countries"
+    },
+    {
+        word: "Bolivia",
+        difficulty: "medium",
+        category: "countries"
+    },
+    {
+        word: "Mauritius",
+        difficulty: "medium",
+        category: "countries"
+    },
+    {
+        word: "Ecuador",
+        difficulty: "medium",
+        category: "countries"
+    },
+    {
+        word: "Finland",
+        difficulty: "medium",
+        category: "countries"
+    },
+    {
+        word: "Guinea",
+        difficulty: "medium",
+        category: "countries"
+    },
+    {
+        word: "Ireland",
+        difficulty: "medium",
+        category: "countries"
+    },
+    {
+        word: "Kuwait",
+        difficulty: "medium",
+        category: "countries"
+    },
+    {
+        word: "Uzbekistan",
+        difficulty: "hard",
+        category: "countries"
+    },
+    {
+        word: "Turkmenistan",
+        difficulty: "hard",
+        category: "countries"
+    },
+    {
+        word: "Bangladesh",
+        difficulty: "hard",
+        category: "countries"
+    },
+    {
+        word: "Seychelles",
+        difficulty: "hard",
+        category: "countries"
+    },
+    {
+        word: "Philippines",
+        difficulty: "hard",
+        category: "countries"
+    },
+    {
+        word: "Mozambique",
+        difficulty: "hard",
+        category: "countries"
+    },
+    {
+        word: "Liechtenstein",
+        difficulty: "hard",
+        category: "countries"
+    },
+    {
+        word: "Kyrgyzstan",
+        difficulty: "hard",
+        category: "countries"
+    },
+    {
+        word: "Eswatini",
+        difficulty: "hard",
+        category: "countries"
+    },
+    {
+        word: "Djibouti",
+        difficulty: "hard",
+        category: "countries"
     },
 ]
