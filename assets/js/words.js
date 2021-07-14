5cdb866c4b2036bf639af06f1eaa2ac3a6afb90f
Yorick van den Hurk <nowane@gmail.com>
2021-07-14 13:51:08 +0000

    Animals-easy in normal js-object-format

---
+++ assets/js/words.js
@@ -0,0 +1,58 @@
+const animals = [
+    {
+    word: "lion",
+    difficulty: "easy",
+    category: "animals"
+},
+{
+    word: "sheep",
+    difficulty: "easy",
+    category: "animals"
+},
+{
+    word: "wolf",
+    difficulty: "easy",
+    category: "animals"
+},
+{
+    word: "tiger",
+    difficulty: "easy",
+    category: "animals"
+},
+{
+    word: "dog",
+    difficulty: "easy",
+    category: "animals"
+},
+{
+    word: "ant",
+    difficulty: "easy",
+    category: "animals"
+},
+{
+    word: "cat",
+    difficulty: "easy",
+    category: "animals"
+},
+{
+    word: "fox",
+    difficulty: "easy",
+    category: "animals"
+},
+{
+    word: "pig",
+    difficulty: "easy",
+    category: "animals"
+}, {
+    word: "cow",
+    difficulty: "easy",
+    category: "animals"
+},
+{
+    word: "bird",
+    difficulty: "easy",
+    category: "animals"
+},
+
+
+]
