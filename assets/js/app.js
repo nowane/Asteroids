46659e73fd298ee753fc11773bcffa6f9ed6aee7
Yorick van den Hurk <nowane@gmail.com>
2021-07-15 09:05:20 +0000

    Comment on function

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,24 +8,30 @@ let words = [
 ];
 
 // Declared variables
-let word = '';
+let hiddenWord = '';
 let maximumWrong = 9;
 let wrongGuesses = 0;
 let guessedLetters = [];
 let currentWord = null;
 
 
-// Random word generation
+// Random word generation out of words array
 function generateWord() {
-    word = words[Math.floor(Math.random() * words.length)];
+    hiddenWord = words[Math.floor(Math.random() * words.length)];
 }
 
+// Display hidden word on screen 
 function guessedWord() {
-    currentWord = word.split('').map(letter => (guessedLetters.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+    // map() creates new array in hiddenWord populated with the results of calling currentWord
+    currentWord = hiddenWord.split('').map(letter =>
+        // Check if letter excists in array - if positive, points to position
+        (guessedLetters.indexOf(letter) >= 0 ? letter : " _ ")).join('');
 
+    // Display the hidden letters of the generated word as undercores in screen
     document.getElementById('hiddenLetters').innerHTML = currentWord;
 }
 
+// Display maximum allowed wrong guesses
 document.getElementById("maximumWrong").innerHTML = maximumWrong;
 
 generateWord();
